a1eb09eb2781942adf3f969e423a26b33aa7ab00
Samanosukeh <flauberth_94@hotmail.com>
2021-02-22 20:09:26 -0300

    forma padrão

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';//core da biblioteca
+import ReactDOM from 'react-dom'; //arvore de documentos...
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')//busca no html o elemento do id root e dentro dele joga isso aqui acima...
 );
